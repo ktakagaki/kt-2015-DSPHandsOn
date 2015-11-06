18ee0b22e86be7804edab4cdbe35eda1a5836f2e
dowa4213 <dominik.walther@tu-ilmenau.de>
2015-11-06 12:38:22 +0100

    updated coding style

--- MedianFilter/Python/functions.py
+++ MedianFilter/Python/functions.py
@@ -9,171 +9,198 @@ import numpy as _np
 import matplotlib.pylab as _plt
 
 
-
-
-
-#Calculate the the median filtered wave with defined window length 
-def _medianFilter( data, windowLength ): 
-    if (windowLength < len(data)and data.ndim == 1):
-         #creating an array where the filtered values will be saved in
-        tempret = _np.zeros(len(data)-windowLength+1) 
-        #check if the window length is odd or even because with even window length we get an unsynchrone filtered wave                     
-        if windowLength % 2 ==0:                                            
+ 
+def _medianFilter(data, windowLength): 
+    """Calculate the the median filtered wave with defined window length."""
+    if (windowLength < len(data) and data.ndim == 1):
+         # Creating an array where the filtered values will be saved in.
+        tempret = _np.zeros(len(data) - windowLength + 1) 
+        # Check if the window length is odd or even because with 
+        # even window length we get an unsynchrone filtered wave.                     
+        if windowLength % 2 == 0:                                            
             for c in range(0, len(tempret)):
-                #write the values of the median filtered wave in tempret, calculate the median of all values in the window
-                tempret[c] = _np.median( data[ c : c + windowLength +1 ] ) 
+                # Write the values of the median filtered wave in tempret, 
+                # calculate the median of all values in the window.
+                tempret[c] = _np.median(data[c: c + windowLength + 1]) 
             return tempret
         else:
             for c in range(0, len(tempret)):
-                tempret[c] = _np.median( data[ c : c + windowLength ] )
+                tempret[c] = _np.median(data[c: c + windowLength])
             return tempret
     else:
-         raise ValueError("windowLength must be smaller than len(data) and data must be a 1D array")
+         raise ValueError("windowLength must be smaller " + 
+                          "than len(data) and data must be a 1D array")
 
-#
-def _medianFilterPlotterImpl( data, windowLength ):
-    #calculate the filtered wave with the medianFiltered function
+def _medianFilterPlotterImpl(data, windowLength):
+    # Calculate the filtered wave with the medianFiltered function.
     datafiltered = _medianFilter(data, windowLength) 
-    #slice the data array to synchronize both waves           
-    data = data[ windowLength / 2 : - windowLength ] 
-    #cut the filtered wave to the same length as the data wave            
-    datafiltered = datafiltered[ : len(data) ]                   
-    _plt.plot( data )
-    _plt.plot( datafiltered )
-    _plt.plot( data-datafiltered )
+    # Slice the data array to synchronize both waves           
+    data = data[windowLength/2 : -windowLength] 
+    # Cut the filtered wave to the same length as the data wave            
+    datafiltered = datafiltered[:len(data)]                   
+    _plt.plot(data)
+    _plt.plot(datafiltered)
+    _plt.plot(data - datafiltered)
 
-
-#Plot all calculated waves, the sine wave, the filtered wave and the difference between bot waves  
-def medianSinPlot( waveNumber, windowLength ):
-    """ Plots a sine wave, the  medain filtered and the difference between sine and median filtered wave """ 
-    #creating an array with a sine waves
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/128 * 2 * _np.pi * waveNumber), (128 + windowLength / 2, ) )   
+def medianSinPlot(waveNumber, windowLength):
+    """ Plot a sine wave, the  medain filtered and the difference between
+    sine and median filtered wave.
+    """ 
+    # Creating an array with sine waves.
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /128*2*_np.pi*waveNumber),
+                            (128 + windowLength/2, ))   
     _medianFilterPlotterImpl(data,windowLength) 
-    
-    
-#Plot all calculated waves, the sine wave, the filtered wave and the difference between bot waves  
-def medianSinPlotNoised( waveNumber, windowLength ):
-    """ Plots a noised sine wave, the  medain filtered and the difference between sine and median filtered wave """ 
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/128 * 2 * _np.pi * waveNumber), (128 + windowLength / 2, ) )
-    noise = _np.random.normal(0,0.2,(128 + windowLength / 2))
+        
+def medianSinPlotNoised(waveNumber, windowLength):
+    """ Plot a noised sine wave, the  medain filtered and the difference
+    between sine and median filtered wave.
+    """ 
+    data = _np.fromfunction(lambda x: _np.sin((x-windowLength / 2)
+                            /128*2*_np.pi*waveNumber),
+                            (128 + windowLength / 2, ))
+    noise = _np.random.normal(0,0.2,(128 + windowLength/2))
     data = data + noise
     _medianFilterPlotterImpl(data,windowLength)
-
-
-#Calculate the error rate of the filtered wave  with different windowLength and defined wave number       
+       
 def _ErrorRateWindow(data, datafiltered, windowLength):
-     #calculate the difference between the sine wave and the filtered wave
+    """Calculate the error rate of the filtered wave
+    with different windowLength and defined wave number.
+    """
+    # Calculate the difference between the sine wave and the filtered wave.
     errorrate = data-datafiltered                    
     error = []                                        
     errorrate = _np.abs(errorrate)
-    #fill the list with the errorrate and corresponding wave number
+    # Fill the list with the errorrate and corresponding wave number.
     error.append([windowLength ,_np.mean(errorrate)])
-     #zip the error ([1,1],[2,2],[3,3]) = ([1,2,3],[1,2,3])
+     # Zip the error ([1,1],[2,2],[3,3]) = ([1,2,3],[1,2,3]).
     error = zip(*error)                              
     return error
 
-
 def _windowErrorPlotterImpl(data,windowLength):
-    #calculate the filtered wave with the medianFiltered function
+    # Calculate the filtered wave with the medianFiltered function.
     datafiltered = _medianFilter(data, windowLength) 
-    #slice the data array to synchronize both waves        
-    data = data[ windowLength / 2 : - windowLength ] 
-    #cut the filtered wave to the same length as the data wave         
+    # Slice the data array to synchronize both waves.
+    data = data[ windowLength / 2 : - windowLength ]
+    # Cut the filtered wave to the same length as the data wave.
     datafiltered = datafiltered[ : len(data) ]
-    #calculate the error with the ErrorRate function                
+    # Calculate the error with the ErrorRate function.
     error = _ErrorRateWindow(data,datafiltered,windowLength) 
-    _plt.axis([0, windowLength + 1, 0, 1.2])
+    _plt.axis([0, windowLength+1, 0, 1.2])
     _plt.xlabel('Window Length', fontsize = 20)
     _plt.ylabel('Error rate', fontsize = 20)
     _plt.scatter(*error)
 
-
-#Plots the error of the median filter with different windowLength and a defined wave number
-def ErrorPlotWindow( waveNumber,windowLength ):
-    """ Plots the Errror(mean of the absolute from sine wave - filtered wave) of the median filter, with different window lengths and fixed wave number"""
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/128 * 2 * _np.pi * waveNumber), (128 + windowLength / 2, ) )
+def ErrorPlotWindow(waveNumber,windowLength):
+    """ Plot the Errror(mean of the absolute from sine wave - filtered wave)
+    of the median filter, with different window lengths and fixed wave number
+    """
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /128*2*_np.pi*waveNumber), 
+                            (128 + windowLength/2,))
     _windowErrorPlotterImpl(data,windowLength)
     
-
 def ErrorPlotWindowNoised( waveNumber,windowLength ):
-    """ Plots the Errror(mean of the absolute from sine wave - filtered wave) of the median filter, with different window lengths and fixed wave number, sine wave is noised"""
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/128 * 2 * _np.pi * waveNumber), (128 + windowLength / 2, ) )
+    """ Plot the Errror(mean of the absolute from sine wave - filtered wave)
+    of the median filter, with different window lengths and fixed wave number,
+    sine wave is noised
+    """
+    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)
+                             /128 * 2 * _np.pi * waveNumber),
+                             (128 + windowLength / 2, ) )
     noise =_np.random.normal(0,0.5,(128 + windowLength / 2))
     data = data + noise
     _windowErrorPlotterImpl(data,windowLength)
-    
- 
-#Calculate the error rate of the filtered wave  with different wave number and defined window length      
+     
+      
 def _ErrorRateWave(data,datafiltered, waveNumber):
+    """Calculate the error rate of the filtered wave
+    with different wave number and defined window length
+    """
     errorrate = data-datafiltered                   
     error = []                                       
     errorrate = _np.abs(errorrate)
     error.append([waveNumber ,_np.mean(errorrate)])
     error = zip(*error)                             
     return error   
-    
- 
+     
 def _waveErrorPlotterImpl(data,waveNumber,windowLength):
-    #calculate the filtered wave with the medianFiltered function
+    # Calculate the filtered wave with the medianFiltered function.
     datafiltered = _medianFilter(data, windowLength)                  
     data = data[ windowLength / 2 : - windowLength ]
     datafiltered = datafiltered[ : len(data) ]  
-    #calculate the error with the ErrorRate functioN                       
+    # Calculate the error with the ErrorRate function.                      
     error = _ErrorRateWave(data,datafiltered,waveNumber)
-    _plt.axis([0, waveNumber + 1, 0, 1.2])
+    _plt.axis([0, waveNumber+1, 0, 1.2])
     _plt.xlabel('Wave number', fontsize = 20)
     _plt.ylabel('Error rate', fontsize = 20)
     _plt.scatter(*error)
-  
-  
-#Plots the error  of the median filter with different wave number and a defined window length      
+         
 def ErrorPlotWave(waveNumber,windowLength):
-    """ Plots the Errror(mean of the absolute from sine wave - filtered wave) of the median filter, with different wave number and fixed window length"""
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/128 * 2 * _np.pi * waveNumber), (128 + windowLength / 2, ) )
+    """ Plot the Errror(mean of the absolute from sine wave - filtered wave)
+    of the median filter, with different wave number and fixed window length
+    """
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /128* 2*_np.pi*waveNumber),
+                            (128 + windowLength/2,))
     _waveErrorPlotterImpl(data,waveNumber,windowLength)
-
-    
-#Plots the error  of the median filtered, noised with different wave number and a defined window length     
-def ErrorplotWaveNoised( waveNumber, windowLength ):
-    """ Plots the Errror(mean of the absolute from sine wave - filtered wave) of the median filter, with different wave number and fixed window length, sine wave is noised"""
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/128 * 2 * _np.pi * waveNumber), (128 + windowLength / 2, ) )
-    noise = _np.random.normal(0,0.5,(128 + windowLength / 2))
+         
+def ErrorplotWaveNoised(waveNumber, windowLength):
+    """ Plot the Errror(mean of the absolute from sine wave - filtered wave)
+    of the median filter, with different wave number and fixed window length,
+    sine wave is noised
+    """
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /128*2*_np.pi*waveNumber),
+                            (128 + windowLength/2,))
+    noise = _np.random.normal(0, 0.5, (128 + windowLength/2))
     data = data + noise
     _waveErrorPlotterImpl(data,waveNumber)
-        
 
 
-    
-"""Because of problems with the low number of samples, I define the same functions with 1024 samples instead of 128."""
-              
-#Plot all calculated waves, the sine wave, the filtered wave and the difference between bot waves  
+#Because of problems with the low number of samples, 
+# I define the same functions with 1024 samples instead of 128.
+  
+            
 def medianSinPlot1024( waveNumber, windowLength ):
-    """ Plots a sine wave, the  medain filtered and the difference between sine and median filtered wave with 1024 samples """
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/1024 * 2 * _np.pi * waveNumber), (1024 + windowLength / 2, ) )
+    """ Plot a sine wave, the  medain filtered and the difference
+    between sine and median filtered wave with 1024 samples.
+    """
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /1024*2*_np.pi*waveNumber), 
+                            (1024 + windowLength/2,))
     _medianFilterPlotterImpl(data,windowLength)
 
-
-#Calculate a noised sine wave to get a more realistic wave
-def medianSinPlotNoised1024( waveNumber, windowLength ):
-    """ Plots a noised sine wave, the  medain filtered and the difference between sine and median filtered wave with 1024 samples"""
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/1024 * 2 * _np.pi * waveNumber), (1024 + windowLength / 2, ) )
-    #creating the noise as an array, filled with random numbers, with the same length as the data array
-    noise = _np.random.normal(0,0.2,(1024 + windowLength / 2)) 
-    #generate the noised signal      
+def medianSinPlotNoised1024(waveNumber, windowLength):
+    """ Plot a noised sine wave, the  medain filtered and the difference
+    between sine and median filtered wave with 1024 samples.
+    """
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /1024*2*_np.pi*waveNumber),
+                            (1024 + windowLength/2,))
+    # Creating the noise as an array, filled with random numbers,
+    # with the same length as the data array.
+    noise = _np.random.normal(0, 0.2, (1024 + windowLength/2)) 
+    # Generate the noised signal.      
     data = data + noise                                               
     _medianFilterPlotterImpl(data,windowLength)
 
-
-#Plots the error of the median filter with different windowLength and a defined wave number
 def ErrorPlotWindow1024( waveNumber,windowLength ):
-    """ Plots the Errror(mean of the absolute from sine wave - filtered wave) of the median filter, with different window lengths and fixed wave number, the waves are plotted with 1024"""
-    data = _np.fromfunction(lambda x: _np.sin((x-windowLength / 2)/1024 * 2 * _np.pi * waveNumber), (1024 + windowLength / 2, ))
-    _windowErrorPlotterImpl(data,windowLength)
-    
-      
-#Plots the error  of the median filter with different wave number and a defined window length      
+    """ Plot the Errror(mean of the absolute from sine wave - filtered wave)
+    of the median filter, with different window lengths and fixed wave number,
+    the waves are plotted with 1024 samples.
+    """
+    data = _np.fromfunction(lambda x: _np.sin((x - windowLength/2)
+                            /1024*2*_np.pi*waveNumber),
+                            (1024 + windowLength/2,))
+    _windowErrorPlotterImpl(data, windowLength)
+              
 def ErrorPlotWave1024(waveNumber,windowLength):
-    """ Plots the Errror(mean of the absolute from sine wave - filtered wave) of the median filter, with different wave number and fixed window length, the waves are plotted with 1024"""
-    data = _np.fromfunction( lambda x: _np.sin((x-windowLength / 2)/1024 * 2 * _np.pi * waveNumber), (1024 + windowLength / 2, ) )
-    _waveErrorPlotterImpl(data,waveNumber,windowLength)
-  
+    """ Plot the Errror(mean of the absolute from sine wave - filtered wave)
+    of the median filter, with different wave number and fixed window length,
+    the waves are plotted with 1024 samples
+    """
+    data = _np.fromfunction( lambda x: _np.sin((x - windowLength/2)
+                            /1024*2*_np.pi*waveNumber),
+                            (1024 + windowLength/2,))
+    _waveErrorPlotterImpl(data, waveNumber, windowLength)
--- MedianFilter/Python/gitInformation.py
+++ MedianFilter/Python/gitInformation.py
@@ -1,114 +1,118 @@
 # -*- coding: utf-8 -*-
 """
-This package allows you to add information to your Ipython notebook, if you're working in a git repository.
+This package allows you to add information to your Ipython notebook,
+if you're working in a git repository.
 It is usefull if you want to verify the notebook version.
-This package is collecting all information by its own, so you don't need to set any repository path.
-You also can check the information of other notebooks, when you set the path by your own.
+This package is collecting all information by its own,
+so you don't need to set any repository path.
+You also can check the information of other notebooks,
+when you set the path by your own.
+
+
 Requierments: *local git repository
               *git python package installed
 """
 import os as _os
 import datetime as _datetime
 import sys as _sys
+
 from git import Repo as _Repo
 
 
 def _getFileDirectory():
-    #Set the file directoy  as directory of the currently used file
+    # Set the file directoy  as directory of the currently used file.
     fileDirectory = _os.path.dirname(_os.path.realpath('_file_')) 
     return fileDirectory
 
 def _findGitRepo(fileDirectory):
-    #Check if the file path is a git repository path
+    # Check if the file path is a git repository path.
     if _os.path.exists(fileDirectory + '\\.git') == True:  
         return fileDirectory + '\\.git' 
     else:
-        #set the directory as parent directory
+        # Set the directory as parent directory.
         parentPath = _os.path.abspath(_os.path.join(fileDirectory, _os.pardir))
-        #if directory is the root directory, no git repository exists
+        # If directory is the root directory, no git repository exists.
         if fileDirectory == parentPath:        
             print 'No \.git repository found'
             return None
         else:
-            #repeat the process until you find a git repository or the directory is the root directory
+            # Repeat the process until you find a git repository or 
+            # the directory is the root directory.
             return _findGitRepo(parentPath)   
             
-
-
 def printInformation( filePath = '' ):
-    """Plot important information about your notebook: Date, used Python version, git directory, git commit SHA, current remote and current branch"""
-    if( filePath == '' ):
-        _printInformationImpl( _findGitRepo(_getFileDirectory()), 'this notebook' )
+    """Plot important information about your notebook:
+    Date, used Python version, git directory, git commit SHA,
+    current remote and current branch
+    """
+    if(filePath == ''):
+        _printInformationImpl(_findGitRepo(_getFileDirectory()), 
+                              'this notebook')
     else:
         _printInformationImpl( _findGitRepo(filePath), filePath )
 
 def _printInformationImpl(filePath, targetString):
     mylist = []  
     today = _datetime.date.today()
-    #append the Date into a list, so it can be printed
+    # Append the Date into a list, so it can be printed.
     mylist.append(today) 
-    #represents your git repository path
+    # Represents your git repository path.
     repo = _Repo(filePath) 
-    print( 'Information about ' + targetString )
-    print( '============================================================' )
-    print( "Date: " +  str(mylist[0])  )
-    print( 'Python Version: ' + _sys.version )
-    print( 'Git directory: ' + _findGitRepo(filePath) )
-    print( 'Current git SHA: ' +  repo.commit().hexsha )
-    print( 'Current remote: ' + str( repo.remote() ) )
-    print( 'Current branch: ' + str(repo.active_branch)  )
+    print('Information about ' + targetString)
+    print('============================================================')
+    print("Date: " +  str(mylist[0]))
+    print('Python Version: ' + _sys.version)
+    print('Git directory: ' + _findGitRepo(filePath))
+    print('Current git SHA: ' +  repo.commit().hexsha)
+    print('Current remote: ' + str(repo.remote()))
+    print('Current branch: ' + str(repo.active_branch))
 
-
-### with the following functions you are able to print single informations about the notebook and repository###
+# with the following functions you are able to print single
+# information about the notebook and repository.
 
 def printCurrentBranch(filePath = ''):
-    """Plot information about the current git branch"""
-    if( filePath == '' ):
-        _currentBranchImpl( _findGitRepo(_getFileDirectory()) )
+    """Plot information about the current git branch."""
+    if(filePath == ''):
+        _currentBranchImpl(_findGitRepo(_getFileDirectory()))
     else:
-        _currentBranchImpl( _findGitRepo(filePath) )
+        _currentBranchImpl(_findGitRepo(filePath))
         
 def _currentBranchImpl(filePath):
     repo = _Repo(filePath)
-    #print current branch
-    print( 'Current branch: ' + str(repo.active_branch)  )
-        
-    
-    
+    # Print current branch.
+    print( 'Current branch: ' + str(repo.active_branch))
+          
 def printCurrentGitSHA(filePath = ''):
-    """Plot information about the current git commit hash"""
+    """Plot information about the current git commit hash."""
     if( filePath == '' ):
-        _currentGitSHAImpl( _findGitRepo(_getFileDirectory()) )
+        _currentGitSHAImpl(_findGitRepo(_getFileDirectory()))
     else:
-        _currentGitSHAImpl( _findGitRepo(filePath) )
+        _currentGitSHAImpl(_findGitRepo(filePath))
     
 def _currentGitSHAImpl(filePath):
      repo = _Repo(filePath)
      #print current git commit hash code
-     print( 'Current git SHA: ' +  repo.commit().hexsha ) 
-
-        
+     print( 'Current git SHA: ' +  repo.commit().hexsha) 
+      
 def printCurrentRemote(filePath = ''):
-    """Plot information about the current git remote"""
-    if( filePath == '' ):
-        _currentRemoteImpl( _findGitRepo(_getFileDirectory()) )
+    """Plot information about the current git remote."""
+    if(filePath == ''):
+        _currentRemoteImpl(_findGitRepo(_getFileDirectory()))
     else:
-        _currentRemoteImpl( _findGitRepo(filePath) )
+        _currentRemoteImpl(_findGitRepo(filePath))
 
 def _currentRemoteImpl(filePath):
     repo = _Repo(filePath)
-    #print current remote of your repository
-    print( 'Current remote: ' + str( repo.remote() ) ) 
+    # Print current remote of your repository.
+    print( 'Current remote: ' + str(repo.remote())) 
     
-
-
 def printGitDirectory(filePath = ''):
-    """Plot information about the current git directory"""
+    """Plot information about the current git directory."""
     if( filePath == '' ):
-        _gitDirectoryImpl( _findGitRepo(_getFileDirectory()) )
+        _gitDirectoryImpl(_findGitRepo(_getFileDirectory()))
     else:
-        _gitDirectoryImpl( _findGitRepo(filePath) )
+        _gitDirectoryImpl(_findGitRepo(filePath))
     
 def _gitDirectoryImpl(filePath):
-    #print directory, where the repository is saved
+    # Print directory, where the repository is saved.
     print( 'Git directory: ' + _findGitRepo(filePath))  
